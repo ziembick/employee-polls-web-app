9d6ae2f2b60ce9c819da662dc89905a7254e7917
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-17 20:50:47 -0300

    receive users reducer and actions

--- employee-polls/src/index.js
+++ employee-polls/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from "redux"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
