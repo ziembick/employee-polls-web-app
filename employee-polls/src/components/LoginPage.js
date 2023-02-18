6263f783129d9eabf595132872fe948f6798db9c
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-17 21:33:30 -0300

    login page

--- employee-polls/src/components/LoginPage.js
+++ employee-polls/src/components/LoginPage.js
@@ -1,12 +1,10 @@
-import React from "react";
-import { useState } from "react";
-import { connect } from "react-redux";
-import { _getUsers } from "../utils/_DATA";
-import { authedUser } from "../actions/authedUser";
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { loginUser } from '../actions/authedUser';
 
 function LoginPage({ users, dispatch }) {
   const [selectedUser, setSelectedUser] = useState(null);
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState('');
 
   const handleUserChange = (e) => {
     setSelectedUser(e.target.value);
@@ -22,9 +20,9 @@ function LoginPage({ users, dispatch }) {
     const user = users[selectedUser];
 
     if (user && user.password === password) {
-      dispatch(authedUser(user.id));
+      dispatch(loginUser(user.id));
     } else {
-      alert("Invalid username or password");
+      alert('Invalid username or password');
     }
   };
 
@@ -60,11 +58,79 @@ function LoginPage({ users, dispatch }) {
   );
 }
 
-function mapStateToProps({ users, authedUser }) {
-  return {
-    users,
-    authedUser,
-  };
+function mapStateToProps({ users }) {
+  return { users };
 }
 
 export default connect(mapStateToProps)(LoginPage);
+
+// import React from "react";
+// import { useState } from "react";
+// import { connect } from "react-redux";
+// import { _getUsers } from "../utils/_DATA";
+// import { authedUser } from "../actions/authedUser";
+
+// function LoginPage({ users, dispatch }) {
+//   const [selectedUser, setSelectedUser] = useState(null);
+//   const [password, setPassword] = useState("");
+
+//   const handleUserChange = (e) => {
+//     setSelectedUser(e.target.value);
+//   };
+
+//   const handlePasswordChange = (e) => {
+//     setPassword(e.target.value);
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     const user = users[selectedUser];
+
+//     if (user && user.password === password) {
+//       dispatch(authedUser(user.id));
+//     } else {
+//       alert("Invalid username or password");
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Login</h2>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label htmlFor="user-select">Select a user:</label>
+//           <select id="user-select" onChange={handleUserChange}>
+//             <option value="">Select a user...</option>
+//             {Object.keys(users).map((userId) => (
+//               <option key={userId} value={userId}>
+//                 {users[userId].name}
+//               </option>
+//             ))}
+//           </select>
+//         </div>
+//         <div>
+//           <label htmlFor="password-input">Password:</label>
+//           <input
+//             type="password"
+//             id="password-input"
+//             value={password}
+//             onChange={handlePasswordChange}
+//           />
+//         </div>
+//         <button type="submit" disabled={!selectedUser || !password}>
+//           Login
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// function mapStateToProps({ users, authedUser }) {
+//   return {
+//     users,
+//     authedUser,
+//   };
+// }
+
+// export default connect(mapStateToProps)(LoginPage);
