60942ddee203b3d8184fda266b12471d745c447f
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-17 21:26:13 -0300

    authedUser reducer

--- employee-polls/src/reducers/authedUser.js
+++ employee-polls/src/reducers/authedUser.js
@@ -1,9 +1,11 @@
-import { SET_AUTHED_USER } from "../actions/authedUser";
+import { LOGIN_USER, LOGOUT_USER } from '../actions/authedUser';
 
 export default function authedUser(state = null, action) {
   switch (action.type) {
-    case SET_AUTHED_USER:
-      return action.id;
+    case LOGIN_USER:
+      return action.userId;
+    case LOGOUT_USER:
+      return null;
     default:
       return state;
   }
