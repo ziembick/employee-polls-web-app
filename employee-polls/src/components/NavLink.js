0c9eb16de9090bbf97bcdb56e128c9dc7a430785
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-20 19:20:44 -0300

    created navlink

---
+++ employee-polls/src/components/NavLink.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavLink () {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a>OIOI</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavLink
