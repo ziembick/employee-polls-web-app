91de6074b48106730bb894b36ce19dd12b6ed72c
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-20 19:26:22 -0300

    Home page

--- employee-polls/src/components/HomePage.js
+++ employee-polls/src/components/HomePage.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { getUnansweredQuestions, getAnsweredQuestions } from "../utils/helpers";
 
-function HomePage () {
-    return (
-        <div>
-            <h1>OIOII</h1>
-        </div>
-    )
+function HomePage({ authedUser, questions }) {
+  const [showUnanswered, setShowUnanswered] = useState(true);
+
+  const handleToggle = () => {
+    setShowUnanswered(!showUnanswered);
+  };
+
+  const filteredQuestions = showUnanswered
+    ? getUnansweredQuestions(questions, authedUser)
+    : getAnsweredQuestions(questions, authedUser);
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/add" activeClassName="active">
+              New Poll
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/leaderboard" activeClassName="active">
+              Leaderboard
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <h2>Welcome, {authedUser.name}</h2>
+      <div>
+        <button onClick={handleToggle}>
+          {showUnanswered ? "View Answered" : "View Unanswered"}
+        </button>
+      </div>
+      <ul>
+        {filteredQuestions.map((question) => (
+          <li key={question.id}>
+            <div>{question.author} asks:</div>
+            <div>Would you rather {question.optionOne.text} or {question.optionTwo.text}?</div>
+            <div>Category: {showUnanswered ? "Unanswered" : "Answered"}</div>
+            <NavLink to={`/questions/${question.id}`}>View Details</NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default HomePage
+function mapStateToProps({ authedUser, questions }) {
+  return {
+    authedUser,
+    questions: Object.values(questions),
+  };
+}
+
+export default connect(mapStateToProps)(HomePage);
