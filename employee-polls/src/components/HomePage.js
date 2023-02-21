c38c143569dcb0f1cd7f9d56098e1f4c04a36714
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-21 12:58:28 -0300

    home page

--- employee-polls/src/components/HomePage.js
+++ employee-polls/src/components/HomePage.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { questions } from "../utils/_DATA";
-import { getUnansweredQuestions, getAnsweredQuestions } from "../utils/helpers";
+import { questions, users } from "../utils/_DATA";
 
-function HomePage({ authedUser}) {
+import { getUnansweredQuestions, getAnsweredQuestions } from "../utils/helpers";
+import Nav from "./Nav";
+
+function HomePage({ authedUser }) {
   const [showUnanswered, setShowUnanswered] = useState(true);
 
   const handleToggle = () => {
@@ -17,25 +19,7 @@ function HomePage({ authedUser}) {
 
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/" exact activeClassName="active">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/add" activeClassName="active">
-              New Poll
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/leaderboard" activeClassName="active">
-              Leaderboard
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <Nav />
       <h2>Welcome, </h2>
       <div>
         <button onClick={handleToggle}>
@@ -46,7 +30,10 @@ function HomePage({ authedUser}) {
         {filteredQuestions.map((question) => (
           <li key={question.id}>
             <div>{question.author} asks:</div>
-            <div>Would you rather {question.optionOne.text} or {question.optionTwo.text}?</div>
+            <div>
+              Would you rather {question.optionOne.text} or{" "}
+              {question.optionTwo.text}?
+            </div>
             <div>Category: {showUnanswered ? "Unanswered" : "Answered"}</div>
             <NavLink to={`/questions/${question.id}`}>View Details</NavLink>
           </li>
